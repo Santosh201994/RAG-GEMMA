0efb7e00d754db04603bb4df924e1b007ebf32e3
Santosh201994 <161206769+Santosh201994@users.noreply.github.com>
2024-08-06 15:33:31 +0000

    two

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+os.environ['GOOGLE_API_KEY']=os.getenv("GOOGLE_API_KEY")
+
+st.title("Gemma Model Document Q&A")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="Gemma-7b-it")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+Answer the question based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+    """
+)
+
+def vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model="model/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./us_census")
+        st.session_state.docs=st.session_state.loader.load()
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlp=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.sess)
